49cf1496510cffe759735b56722c774cfd5e0398
Aaron John B. Sayoc <aaronjohn.sayoc@gmail.com>
2019-01-19 16:11:43 +0800

    header

--- app/views/home.php
+++ app/views/home.php
@@ -4,9 +4,9 @@
 
 	<header class="container-fluid p-0 m-0">
 		<div id="hdr-image" class="jumbotron jumbotron-fluid text-center m-0">
-		  <div class="container">
-		    <h1 id="hdr-title" class="display-4">The Best Cupcakes Ever!</h1>
-		    <p id="hdr-slogan" class="lead">Only the finest ingredients are used.</p>
+		  <div class="container py-5">
+		    <h1 id="hdr-title" class="display-4 text-white">The Best Cupcakes Ever!</h1>
+		    <p id="hdr-slogan" class="lead text-white">Only the finest ingredients are used.</p>
 		    <a class="btn btn-outline-light btn-lg" href="#">Shop Now</a>
 		  </div>
 		</div>
