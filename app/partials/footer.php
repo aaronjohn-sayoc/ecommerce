01594d9d54fa4b205ae6bf3a23b1550b8ec669a5
Aaron John B. Sayoc <aaronjohn.sayoc@gmail.com>
2019-01-19 14:56:46 +0800

    google font

--- app/partials/footer.php
+++ app/partials/footer.php
@@ -1,6 +1,6 @@
 <footer class="row bg-dark fixed-bottom py-2 justify-content-center">
 	<small class="text-white">
-		DISCLAIMER: This site is for educational purposes only. I do not own nor claim to own any content within this site. &copy;2019 Tapsimeals Foodie Inc.
+		DISCLAIMER: This site is for educational purposes only. I do not own nor claim to own any content within this site. &copy;2019 Crown Bakery Inc.
 	</small>	
 </footer>
 
