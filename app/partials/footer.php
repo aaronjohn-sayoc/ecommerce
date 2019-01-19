9f4510faaad3a4f17babdb62de0e5628b70f1fd0
Aaron John B. Sayoc <aaronjohn.sayoc@gmail.com>
2019-01-19 15:45:47 +0800

    header fix

--- app/partials/footer.php
+++ app/partials/footer.php
@@ -1,8 +1,8 @@
 <footer class="container-fluid">
-	<footer class="row">
-		<footer class="col-12 bg-primary fixed-bottom py-2 justify-content-center">
+	<footer class="row fixed-bottom py-2 justify-content-center">
+		<footer class="col-12 bg-primary">
 			<small class="text-white">
-				DISCLAIMER: This site is for educational purposes only. I do not own nor claim to own any content within this site. &copy;2019 Crown Bakery Inc.
+				DISCLAIMER: This site is for educational purposes only. I do not own nor claim to own any content within this site. &copy; 2019 Crown Bakery Inc.
 			</small>					
 		</footer>	
 	</footer>
