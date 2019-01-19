72fa6b9c93b0c4c95e75696809a4d902cde7a8d4
Aaron John B. Sayoc <aaronjohn.sayoc@gmail.com>
2019-01-19 16:36:00 +0800

    header

--- app/views/home.php
+++ app/views/home.php
@@ -2,18 +2,16 @@
 
 <?php function get_page_content() { ?>
 
-	<header class="container-fluid p-0 m-0">
-		<div id="hdr-image" class="jumbotron jumbotron-fluid text-center m-0">
-			<div class="container pt-3">
+	<header id="hdr-image" class="jumbotron jumbotron-fluid text-center m-0 p-0">
+			<div class="container pt-5">
 		   <h1 id="hdr-title" class="display-3 pt-3 text-white">The Best Cupcakes Ever!</h1>		
 			</div>
-			<div class="container pt-3">
+			<div class="container pt-5">
 		   <p id="hdr-slogan" class="lead text-white">Only the finest ingredients are used.</p>		
 			</div>
-			<div class="container pt-3">
+			<div class="container pt-5">
 		   <a class="btn btn-outline-light btn-lg" href="#">Shop Now</a>			
 			</div>
-		</div>
 	</header> <!-- end of header -->
 
 
