58f288ad77159b0d1793648c975e7d15961ec4c4
Aaron John B. Sayoc <aaronjohn.sayoc@gmail.com>
2019-01-19 15:43:21 +0800

    footer change

--- app/partials/footer.php
+++ app/partials/footer.php
@@ -1,7 +1,11 @@
-<footer class="row bg-dark fixed-bottom py-2 justify-content-center">
-	<small class="text-white">
-		DISCLAIMER: This site is for educational purposes only. I do not own nor claim to own any content within this site. &copy;2019 Crown Bakery Inc.
-	</small>	
+<footer class="container-fluid">
+	<footer class="row">
+		<footer class="col-12 bg-primary fixed-bottom py-2 justify-content-center">
+			<small class="text-white">
+				DISCLAIMER: This site is for educational purposes only. I do not own nor claim to own any content within this site. &copy;2019 Crown Bakery Inc.
+			</small>					
+		</footer>	
+	</footer>
 </footer>
 
 
