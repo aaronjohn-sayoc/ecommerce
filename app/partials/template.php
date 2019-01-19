13f7a76d37f2e8c6d90100278fefaf841e19924e
Aaron John B. Sayoc <aaronjohn.sayoc@gmail.com>
2019-01-19 14:33:59 +0800

    changed title favicon

--- app/partials/template.php
+++ app/partials/template.php
@@ -7,7 +7,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, shrink-to-fit=no">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
 
-	<title>Tapsimeals Foodie</title>
+	<title>Crown Bakery</title>
+
+	<!-- favicon -->
+	<link rel="shortcut icon" type="image/png" href="../assets/images/crownbakery.png"/>
 
 	<!-- fontawesome -->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
