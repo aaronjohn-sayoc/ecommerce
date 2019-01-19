3319e9f23af4fd069273185775b5551bc43f8562
Aaron John B. Sayoc <aaronjohn.sayoc@gmail.com>
2019-01-19 17:48:18 +0800

    my new gallery

--- app/views/home.php
+++ app/views/home.php
@@ -38,8 +38,8 @@
 		            <a href="../assets/images/hello-i-m-nik-1094728-unsplash.jpg?image=5" data-toggle="lightbox" data-gallery="bakery-gallery" data-title="Cupcake Variety" class="col-sm-4 mx-0 px-0">
 		                <img src="../assets/images/hello-i-m-nik-1094728-unsplash.jpg?image=5" class="img-fluid">
 		            </a>
-		            <a href="../assets/images/max-panama-387824-unsplash?image=6" data-toggle="lightbox" data-gallery="bakery-gallery" data-title="Chocolate Cake Strawberry Desert" class="col-sm-4 mx-0 px-0">
-		                <img src="../assets/images/max-panama-387824-unsplash?image=6" class="img-fluid">
+		            <a href="../assets/images/erol-ahmed-260730-unsplash.jpg?image=6" data-toggle="lightbox" data-gallery="bakery-gallery" data-title="Baking Cookie Homemade" class="col-sm-4 mx-0 px-0">
+		                <img src="../assets/images/erol-ahmed-260730-unsplash.jpg?image=6" class="img-fluid">
 		            </a>
 		        </div>
 		    </div>
