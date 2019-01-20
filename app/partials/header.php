2586ddae48e784ad7b54256a2c01935567f1d713
Aaron John B. Sayoc <aaronjohn.sayoc@gmail.com>
2019-01-20 16:39:09 +0800

    header active

--- app/partials/header.php
+++ app/partials/header.php
@@ -5,7 +5,7 @@ function echoActiveClassIfRequestMatches($requestUri)
     $current_file_name = basename($_SERVER['REQUEST_URI'], ".php");
 
     if ($current_file_name == $requestUri)
-        echo 'class="active"';
+        echo 'active';
 }
 
 ?>
@@ -26,15 +26,15 @@ function echoActiveClassIfRequestMatches($requestUri)
 					if(!isset($_SESSION['user']) || (isset($_SESSION['user'])) && ($_SESSION['user']['roles_id'] ==2)) {
 				?>
 
-				<li <?=echoActiveClassIfRequestMatches("home")?> class="nav-item mx-1">
+				<li class="nav-item mx-1 <?=echoActiveClassIfRequestMatches("home")?>">
 					<a class="nav-link" href="./home.php"> Home </a>
 				</li>
 
-				<li <?=echoActiveClassIfRequestMatches("catalog")?> class="nav-item mx-1">
+				<li class="nav-item mx-1 <?=echoActiveClassIfRequestMatches("catalog")?>">
 					<a class="nav-link" href="./catalog.php"> Shop </a>
 				</li>
 
-				<li class="nav-item mx-1">
+				<li class="nav-item mx-1 <?=echoActiveClassIfRequestMatches("cart")?>">
 					<a class="nav-link" href="./cart.php"><i class="fas fa-shopping-cart"></i> Cart <span class="badge badge-primary text-white" id="cart-count">
 						<?php
 							if (isset($_SESSION['cart'])) {
@@ -48,15 +48,15 @@ function echoActiveClassIfRequestMatches($requestUri)
 
 			<?php } elseif(isset($_SESSION['user']) && ($_SESSION['user']['roles_id']==1)) { ?>
 
-				<li class="nav-item mx-1">
+				<li class="nav-item mx-1 <?=echoActiveClassIfRequestMatches("items")?>">
 					<a href="./items.php" class="nav-link">Items</a>
 				</li>
 
-				<li class="nav-item mx-1">
+				<li class="nav-item mx-1 <?=echoActiveClassIfRequestMatches("orders")?>">
 					<a href="./orders.php" class="nav-link">Orders</a>
 				</li>
 
-				<li class="nav-item mx-1">
+				<li class="nav-item mx-1 <?=echoActiveClassIfRequestMatches("users")?>">
 					<a href="./users.php" class="nav-link">Users</a>
 				</li>
 				
@@ -64,21 +64,21 @@ function echoActiveClassIfRequestMatches($requestUri)
 
 				<?php if(isset($_SESSION['user'])) { ?>
 
-				<li class="nav-item mx-1">
+				<li class="nav-item mx-1 <?=echoActiveClassIfRequestMatches("profile")?>">
 					<a class="nav-link" href="./profile.php">Welcome, <?php echo $_SESSION['user']['firstname']; ?> </a>
 				</li>
 
-				<li class="nav-item mx-1">
+				<li class="nav-item mx-1 <?=echoActiveClassIfRequestMatches("logout")?>">
 					<a class="nav-link" href="../controllers/logout.php"> Logout </a>
 				</li>
 
 				<?php } else { ?>
 
-				<li class="nav-item mx-1">
+				<li class="nav-item mx-1 <?=echoActiveClassIfRequestMatches("login")?>">
 					<a class="nav-link" href="./login.php"> Login </a>
 				</li>
 				
-				<li class="nav-item mx-1">
+				<li class="nav-item mx-1 <?=echoActiveClassIfRequestMatches("register")?>">
 					<a class="nav-link" href="./register.php"> Register </a>
 				</li>
 
